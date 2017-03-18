40ab809baf133009deabff794a03674175203b4c
Matyas Korom <korommatyi@gmail.com>
2017-03-18 08:32:31 +0100

    Use use-package and add basic scala support

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 (setq-default tab-width 2)
 (setq js-indent-level 2)
 (setq python-indent-offset 2)
+(setq css-indent-offset 2)
 (show-paren-mode t)
 (global-linum-mode 1)
 (setq column-number-mode t)
@@ -15,12 +16,28 @@
 ; add packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
-(package-install 'smartscan)
-(package-install 'monokai-theme)
-(package-install 'fill-column-indicator)
-(package-install 'magit)
+(package-install 'use-package)
+(require 'use-package)
+(use-package scala-mode
+  :ensure t
+  :pin melpa-stable
+  :interpreter ("scala" . scala-mode))
+(use-package smartscan
+  :ensure t
+  :pin melpa)
+(use-package monokai-theme
+  :ensure t
+  :pin melpa)
+(use-package fill-column-indicator
+  :ensure t
+  :pin melpa)
+(use-package magit
+  :ensure t
+  :pin melpa)
 
 (smartscan-mode 1)
 (load-theme 'monokai t)
@@ -30,7 +47,22 @@
 (setq-default fill-column 100)
 (add-hook 'python-mode-hook (lambda () (fci-mode 1)))
 (add-hook 'makrdown-mode-hook (lambda () (fci-mode 1)))
+(add-hook 'scala-mode-hook (lambda () (fci-mode 1)))
 (add-to-list 'default-frame-alist '(height . 60))
 (add-to-list 'default-frame-alist '(width . 120))
 
 (global-set-key (kbd "C-x g") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit fill-column-indicator monokai-theme smartscan))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
