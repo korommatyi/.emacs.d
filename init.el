91ab7610e3efa2873496b99494c160c0bd12d95e
Matyas Korom <matyas.korom@lynxanalytics.com>
2017-06-30 11:32:33 +0200

    Rebind other-window and nicify

--- init.el
+++ init.el
@@ -4,16 +4,19 @@
 (setq python-indent-offset 2)
 (setq css-indent-offset 2)
 (show-paren-mode t)
-(global-linum-mode 1)
-(setq column-number-mode t)
 (put 'upcase-region 'disabled nil)
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (setq inhibit-startup-message t inhibit-startup-echo-area-message t)
-(define-key global-map (kbd "RET") 'newline-and-indent)
 
-; add packages
+
+;; rebound keys
+(define-key global-map (kbd "RET") 'newline-and-indent)
+(define-key global-map (kbd "M-o") 'other-window)
+
+
+;; add packages
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
@@ -28,7 +31,8 @@
   :interpreter ("scala" . scala-mode))
 (use-package smartscan
   :ensure t
-  :pin melpa)
+  :pin melpa
+  :config (smartscan-mode 1))
 (use-package monokai-theme
   :ensure t
   :pin melpa)
@@ -37,7 +41,9 @@
   :pin melpa)
 (use-package magit
   :ensure t
-  :pin melpa)
+  :pin melpa
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status))
 (use-package exec-path-from-shell
   :ensure t
   :pin melpa
@@ -47,17 +53,21 @@
   :pin melpa
   :config (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
 
-(smartscan-mode 1)
+
+;; style
 (load-theme 'monokai t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
+(global-linum-mode 1)
+(setq column-number-mode t)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
 (setq-default fill-column 100)
 (add-hook 'python-mode-hook (lambda () (fci-mode 1)))
 (add-hook 'makrdown-mode-hook (lambda () (fci-mode 1)))
 (add-hook 'scala-mode-hook (lambda () (fci-mode 1)))
-(add-to-list 'default-frame-alist '(height . 60))
-(add-to-list 'default-frame-alist '(width . 120))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
@@ -66,8 +76,6 @@
   (setq web-mode-code-indent-offset 2)
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-
-(global-set-key (kbd "C-x g") 'magit-status)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -75,7 +83,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit fill-column-indicator monokai-theme smartscan))))
+    (web-mode use-package smartscan scala-mode monokai-theme magit fill-column-indicator exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
