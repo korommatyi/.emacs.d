5c2b6cbc0861335b3da434d454730cd9dd5260ac
Matyas Korom <matyas.korom@lynxanalytics.com>
2019-05-21 16:27:14 +0200

    Jsx support

--- init.el
+++ init.el
@@ -91,7 +91,8 @@
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))
 
 (use-package smex
   :ensure t
